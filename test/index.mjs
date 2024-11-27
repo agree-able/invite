3706710dc6d4f3ecf9e2b388c279b3987335d14b
Ryan Ramage <r@mage.rocks>
2024-11-26 21:00:30 -0700

    simple test

---
+++ test/index.mjs
@@ -0,0 +1,11 @@
+import t from 'tap'
+import { load } from '../index.mjs'
+
+t.test('load where invite is on the config object', async t => {
+  const config = { invite: 'aaaa' }
+  const confirmEnterRoom = (_expectations, _extraInfo) => {}
+  const { invite } = await load(config, confirmEnterRoom)
+  t.ok(invite, 'invite')
+  t.equal(invite, 'aaaa')
+  t.end()
+})
